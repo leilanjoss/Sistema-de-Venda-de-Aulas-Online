9ec57a153a09a8b72b357d7e6ad5036b2c3266c0
3ateusferrari <mateusspier@gmail.com>
2024-05-16 15:02:54 -0300

    inscricao e controlador feito até uma parte

---
+++ controller/controladorInscricao.py
@@ -0,0 +1,16 @@
+#Verificar caminho e nome de alguns imports já que eu fiz antes de criar os arquivos
+from view.telaListagemCursos import TelaListagemCursos #Verificar
+from view.telaInscricao import TelaInscricao #Verificar
+from controladorSistema import ControladorSistema
+from controladorRelatorio import ControladorRelatorio #Verificar
+from model.inscricao import Inscricao
+
+class ControladorInscricao:
+    def __init__(self, controlador_sistema):
+        self.__inscricoes = []
+        self.__tela_listagem_cursos = TelaListagemCursos()
+        self.__controlador_sistema = controlador_sistema
+    
+    def inserir_inscricao(self, inscricao: Inscricao):
+        if (isinstance(inscricao, Inscricao) and inscricao is not None and inscricao is not self.__inscricoes):
+            
---
+++ model/inscricao.py
@@ -0,0 +1,36 @@
+from controller.controladorCurso import ControladorCurso
+from controller.controladorInscricao import ControladorInscricao
+from model.curso import Curso
+from model.aluno import Aluno
+# Fazer a ligação com  o pontilhado, acho que deve ser pra puxar dos dois
+
+class Inscricao():
+    def __init__(self, curso: Curso, aluno: Aluno, preco_pago: float, data_hora: int):
+        self.__curso = curso
+        self.__aluno = aluno
+        self.__preco_pago =  preco_pago
+        self.__data_hora = data_hora
+ 
+    @property
+    def curso (self):
+        return self.__curso
+
+    @property
+    def aluno (self):
+        return self.__aluno
+
+    @property
+    def preco_pago(self):
+        return self.__preco_pago
+
+    @property
+    def data_hora(self):
+        return self.__data_hora
+
+    @curso.setter
+    def curso(self, curso):
+        self.__curso = curso
+
+    @aluno.setter
+    def aluno(self, aluno):
+        self.__aluno = aluno
